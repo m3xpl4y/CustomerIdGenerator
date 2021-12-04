ba6f9a324b55976d6fe480f7a2f25db0a120c5b6
m3xpl4y <m3xpl4y@gmail.com>
2021-12-04 22:59:24 +0100

    Add project files.

---
+++ Generator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomerIdGenerator
+{
+    public class Generator
+    {
+        List<string> context = new List<string>() { "DX645789", "AC125489", "KL985632" }; //replace this list with a Database!
+        private int Length { get; set; }
+
+        public Generator(List<string> context)
+        {
+            this.context = context;
+        }
+        public Generator(int length, List<string> context)
+        {
+            this.context = context;
+            this.Length = length;
+        }
+        private string GetLetters()
+        {
+            Random rnd = new Random();
+            string customerId = "";
+            int ascii_index = 0;
+
+            for (int i = 0; i < 2; i++)
+            {
+                ascii_index = rnd.Next(65, 91);
+                char upperCaseLetter = Convert.ToChar(ascii_index);
+                customerId += upperCaseLetter;
+            }
+            return customerId;
+        }
+        private string GetNumbers()
+        {
+            Random rnd = new Random();
+            string numbers = "";
+            if (Length == 0)
+            {
+                int[] num = new int[6];
+                for (int i = 0; i < num.Length; i++)
+                {
+                    num[i] = rnd.Next(0, 10);
+                    numbers += num[i].ToString();
+                }
+            }
+            else
+            {
+                int[] num = new int[Length];
+                for (int i = 0; i < num.Length; i++)
+                {
+                    num[i] = rnd.Next(0, 10);
+                    numbers += num[i].ToString();
+                }
+            }
+            return numbers;
+        }
+        private string CreateCUID()
+        {
+            string customerId = "";
+            string letters = GetLetters();
+            string numbers = GetNumbers();
+            customerId = letters + numbers;
+            if (CheckCUID(customerId) == true)
+            {
+                CreateCUID();
+            }
+            return customerId;
+        }
+        private bool CheckCUID(string id)
+        {
+            bool isTaken = false;
+            string customerId = id;
+            var cuidList = context;
+            foreach (var item in cuidList)
+            {
+                if (item == customerId)
+                {
+                    isTaken = true;
+                }
+                else
+                {
+                    isTaken = false;
+                }
+            }
+            return isTaken;
+        }
+        public string NewCuid()
+        {
+            return CreateCUID();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomerIdGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> context = new List<string>() { "DX645789", "AC125489", "KL985632" }; //replace this list with a Database!
+
+            //Generate a CustomerId of 2 Letters and 6 numbers
+            //context replaces a list retrived from the Database
+            //to compare with already assigned CustomerIds in the Database
+            //to not assigned the same one twice
+
+            Generator gen = new Generator(context);
+            Console.WriteLine(gen.NewCuid());
+            Console.WriteLine(gen.NewCuid());
+            
+            //Choose the length your CustomerId by adding first a number for length
+            
+            Generator gen2 = new Generator(9,context);
+            Console.WriteLine(gen2.NewCuid());
+            Console.WriteLine(gen2.NewCuid());
+        }
+    }
+}
